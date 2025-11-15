ca359c772f33dffa133f8e728213531becabeea9
Felix Reissmann <felix.rm.153@gmail.com>
2025-11-15 19:09:08 +0100

    Make C/C++ env variable manipulation manually triggered.

--- bits/c++/clang_default.sh
+++ bits/c++/clang_default.sh
@@ -1,8 +1,11 @@
-if ! command -v clang &> /dev/null; then
-    echo "Clang is not installed. Please install it and try again."
-    return 1
-fi
+function set_clang_as_default_compiler() {
+    if ! command -v clang &> /dev/null; then
+        echo "Clang is not installed. Please install it and try again."
+        return 1
+    fi
 
-export CC=clang
-export CXX=clang++
-export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+    export CC=clang
+    export CXX=clang++
+    export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+    echo "Clang has been set as the default compiler."
+}
--- bits/c++/mold_default.sh
+++ bits/c++/mold_default.sh
@@ -1,6 +1,10 @@
-if ! command -v mold &> /dev/null; then
-    echo "Mold is not installed. Please install it and try again."
-    return 1
-fi
+function set_mold_as_default_linker() {
+    if ! command -v mold &> /dev/null; then
+        echo "Mold is not installed. Please install it and try again."
+        return 1
+    fi
 
-export CXXFLAGS="$CXXFLAGS -fuse-ld=mold"
+    export LD=mold
+    export CXXFLAGS="$CXXFLAGS -fuse-ld=mold"
+    echo "Mold has been set as the default linker."
+}
