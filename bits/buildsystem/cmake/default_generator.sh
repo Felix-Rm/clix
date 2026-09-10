982ecafb8069649558d41fc7bcac5e64861acbfd
Felix Reissmann <felix.rm.153@gmail.com>
2026-09-10 14:35:43 +0200

    Remove warnings for non-invoked features.

--- bits/buildsystem/cmake/default_generator.sh
+++ bits/buildsystem/cmake/default_generator.sh
@@ -1,11 +1,13 @@
-if ! command -v cmake &> /dev/null; then
-    echo "CMake is not installed. Please install it and try again."
-    return 1
-fi
+function set_ninja_as_default_generator() {
+    if ! command -v cmake &> /dev/null; then
+        echo "CMake is not installed. Please install it and try again."
+        return 1
+    fi
 
-if ! command -v ninja &> /dev/null; then
-    echo "Ninja is not installed. Please install it and try again."
-    return 1
-fi
+    if ! command -v ninja &> /dev/null; then
+        echo "Ninja is not installed. Please install it and try again."
+        return 1
+    fi
 
-export CMAKE_GENERATOR=Ninja
+    export CMAKE_GENERATOR=Ninja
+}
